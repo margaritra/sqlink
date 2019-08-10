d0b5001cba417cfcbfeed36219a89c59c47c0b66
Margarita <53777457+margaritra@users.noreply.github.com>
2019-08-10 12:41:32 +0300

    Update class.c

--- C/class.c
+++ C/class.c
@@ -4,15 +4,27 @@
 
 
 int my_str_cmp(const char * str1, const char * str2);
+char *my_str_cpy( char * str1, const char * str2);
+char *my_str_cat( char * str1, const char * str2);
 
 int main () {
 
-   char str1[12] = "Hello";
-   char str2[12] = "Hllo";
    int result = 0;
+   char *result1 ;
+   char *result2;
+
+   char str1[50] = "wwww";
+   char str2[50] = "zczxczxc";
 
    result = my_str_cmp(str1,str2);
    printf("%d\n",result);
+   
+
+   result1 = my_str_cpy(str1,str2);
+   printf("%s\n",result1);
+
+   result2 = my_str_cat(str1,str2);
+   printf("result2 :%s\n",result2);
    
    return 0;
 }
@@ -40,3 +52,36 @@ int my_str_cmp(const char * str1, const char * str2)
 
 }
 
+char *my_str_cpy( char * str1, const char * str2)
+{
+	if(strcmp(str2, "")!=0)
+	{
+		for (int i = 0;i<strlen(str2);i++)
+		{
+			str1[i] = str2[i];
+			if (i == (strlen(str1)-1))
+			{
+				str1[i] = '\0';
+			}
+		}
+	}
+	return str1;
+}
+
+
+char *my_str_cat( char * str1, const char * str2)
+{
+	int j;
+	
+	if(strcmp(str2, "")!=0)
+	{		
+		j = strlen(str1);	
+		for(int k = 0;k<strlen(str2);k++)
+		{
+			str1[j] = str2[k];
+			j++;
+		}
+	}
+	return str1;
+	
+}
