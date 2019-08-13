6cb880af8d9acef4b4ba4d16e1e3530a9421b826
Margarita <53777457+margaritra@users.noreply.github.com>
2019-08-13 16:26:50 +0300

    Update functions.c

--- C/05_Calendar/functions.c
+++ C/05_Calendar/functions.c
@@ -19,16 +19,16 @@ calendar_t* create_ad(int capacity)
 
 meeting_t* create_meet()
 {
-	meeting_t meeting ;
+	meeting_t* meeting ;
 	float start;
 	float end ;
 	int room;
-    meeting = malloc(sizeof(meeting_t));
+    	meeting = malloc(sizeof(meeting_t));
 	if(meeting!=NULL)
 	{
-		printf("Enter the start ,end and room number: ");    
-		scanf("%d",&start,&end,&room); 
-		//bdikat from 0 to 24
+		printf("Enter the start ,end and room number: \n");    
+		scanf("%f%f%d",&start,&end,&room); 
+		//bdika from 0 to 24
 		meeting->start_h = start;
 		meeting->end_h = end;
 		meeting->room = room;
@@ -37,78 +37,103 @@ meeting_t* create_meet()
 	return meeting;
 }
 
-int insert_appointment(calendar_t calendar , meeting_t* meet)
-{
-	
-	temp = calendar->meeting;
-
-	if(calendar->meeting!=NULL && calendar!=NULL && meet!=NULL)
-		{
-			if(calendar->index==0)//first insert
-			{
-				calendar->meeting[0] = meet;//10 -11
-				(calendar->index)++;
-			}
-			else //x inserts
-			{
-				if(calendar->capacity < calendar->index)//if index>capacity
-				{
-					calendar->meeting = realloc(calendar->capacity*2,sizeof(intPtr));
-					calendar->capacity *= 2;
-				}
-				
-				if(calendar->meeting!=NULL)
-				{
-					for(int i=0;i<calendar->index;i++){
-						if(calendar->meeting[i]->end_h < meet->end_h){
-							shift_right(&calendar->meeting[i],(calendar->index)-i)
-							calendar->meeting[i] = meet;
-							calendar->index++;
-							return 1;
-						}
-					}
-					calendar->meeting[calendar->index] = meet;
-					calendar->index++;
-				}
-				else
-				{
-					calendar->meeting = temp;
-					return 0;
-				}						
-			}
-		}
-	}
-	return 0;
-}
-
-void shift_right(meeting_t* meets,int size)
+void shift_right(calendar_t* calebdar,int i)
 {
 	meeting_t temp = meets[0];
 	meeting_t temp2;
 
-	meets[0] = NULL;
-	for(int i=1;i<size;i++){
+	//meets[0] = NULL;
+	for(int i=1;i<size;i++)
+	{
 		temp2 = meets[i];
 		meets[i] = temp;
 		temp = temp2;
 	}
 }
 
+
+int insert_appointment(calendar_t *calendar , meeting_t* meet)
+{
+	intPtr* temp;
+	temp = calendar->meeting;
+
+	if(calendar->meeting!=NULL && calendar!=NULL && meet!=NULL)
+	{
+		if(calendar->index==0)//first insert
+		{
+			calendar->meeting[0] = meet;//10 -11
+			calendar->index++;
+			return 1;
+		}
+
+		else //x inserts
+		{
+			if(calendar->capacity <= calendar->index)//if index>meetingsAmount
+			{
+				calendar->meeting = realloc(calendar->meet,calendar->capacity*2*sizeof(intPtr));//(ptr, size)
+				calendar->capacity *= 2;
+       		}
+
+			if(calendar->meeting!=NULL)
+			{
+				for(int i=0;i<calendar->index;i++)
+				{	
+					if(i == calendar->index-1 && calendar->meeting[i]->end_h > meet->start_h)
+					{
+				
+						calendar->meeting[i] = meet;
+						calendar->index++;
+						return 1;
+
+					}
+	
+					else if(calendar->meeting[i]->end_h < meet->start_h && calendar->meeting[i+1]->start_h > meet->end_h )
+					{
+						shift_right(calendar->meeting[i],(calendar->index)-i);
+						calendar->meeting[i] = meet;
+						calendar->index++;
+						return 1;
+					}
+					else if(calendar->meeting[i]->start_h > meet->end_h && i == 0)
+					{
+						shift_right(calendar->meeting[i],(calendar->index)-i);
+						calendar->meeting[i] = meet;
+						calendar->index++;
+						return 1;
+					}
+					
+				}
+			calendar->meeting[calendar->index-1] = meet;//if index>meetingsAmount && if index<meetingsAmount
+			calendar->index++;
+			return 1;
+			}
+			else
+			{
+				calendar->meeting = temp;
+				return 0;
+			}
+		}
+    }
+    return 0;
+}
+
+
+
 void print_AD(calendar_t * calendar)
 {
 	if(calendar!=NULL)
 	{
-		for(int i=1;i<calendar->index;i++)
+		for(int i=0;i<calendar->index;i++)
 		{
 			printf("*******************************************\n");
-			printf("start :%d\n",calendar->meeting[i]->start_h);
-			printf("end :%d\n",calendar->meeting[i]->end_h);
-			printf("room :%d\n",calendar->meeting[i]->room);
+			printf("start :%f\n",calendar->meeting[i]->start_h);
+			printf("end :  %f\n",calendar->meeting[i]->end_h);
+			printf("room : %d\n",calendar->meeting[i]->room);
 			printf("*******************************************\n");
 		}
 	}
 }
-
+/*
 void destroy_da(calendar_t * calendar)
 {
 	if((calendar->meeting)!=0)
@@ -117,6 +142,7 @@ void destroy_da(calendar_t * calendar)
 		{
 			free(calendar->meeting[i]);
 		}
+		free(calendar->meeting);
 	}
 	if (calendar!=0)
 	{
@@ -125,7 +151,7 @@ void destroy_da(calendar_t * calendar)
 	
 }
 
-
+*/
 
 
 
