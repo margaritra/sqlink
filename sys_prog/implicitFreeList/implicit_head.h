e4cbe30788763a7f94c87d9418b7ced40e4987c9
Margarita <53777457+margaritra@users.noreply.github.com>
2019-09-04 18:35:24 +0300

    Update implicit_head.h

--- sys_prog/implicitFreeList/implicit_head.h
+++ sys_prog/implicitFreeList/implicit_head.h
@@ -1,9 +1,8 @@
-
 #ifndef _MYhead_H_
 #define _MYhead_H_
 
 
 void* memInit(void* buffer,int size);
-void* memAlloc(void* header,int size);
-void* memFree(void* header);
+void *memAlloc(void* header,int size);
+int memFree(void* header);
 #endif
