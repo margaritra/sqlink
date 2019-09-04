e7429633e8dee76be02971fecbb6a486a83af1b0
Margarita <53777457+margaritra@users.noreply.github.com>
2019-09-04 18:35:01 +0300

    Update impl_Test.c

--- sys_prog/implicitFreeList/impl_Test.c
+++ sys_prog/implicitFreeList/impl_Test.c
@@ -1,20 +1,59 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include "implicit_head.h"
-
-
-
+/*
+void printStatusAlloc(void * yourlocation)
+{
+	if((int*)yourlocation==0)
+	{
+		printf("ERROR ALLOCATION!!!\n");
+	}
+	else
+	{
+		printf("SUCCESSFULLY ALLOCATED!!!\n");
+	}
+}
+*/
 void test(char *list,int size)
 {
 	char * header;
-	char * yourlocation;
+	int yourlocation;
+	
+
 	header = memInit(list,size);
-	yourlocation = memAlloc(header,15);
+
+	int mysize = 5;
+
+	header = memAlloc(header,mysize);
+	if(header==0)
+	{
+		printf("ERROR ALLOCATION!!!\n");
+	}
+	else
+	{
+		printf("SUCCESSFULLY ALLOCATED!!!\n");
+	}
+
+	//mysize = 5;
+
+	//header = memAlloc(header,mysize);
+	int statusFree = memFree(header);
+
+	if(statusFree==0)
+	{
+		printf("FREE ERROR!!!\n");
+	}
+	else
+	{
+		printf("SUCCESSFULLY FREE!!!\n");
+	}
+	
 }
 
 int main()
 {
-	int size = 1000;
-	char *list;
+	int size = 16;
+	char *list = malloc(size+4);
 	test(list,size);
 	return 0;
 }
